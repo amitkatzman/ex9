8f405411e73aff05debf52ad467d4e1320472fe7
AmitKatzman <amitkatzman1@gmail.com>
2025-02-10 20:59:11 +0200

    updated q1.sh

--- q1.sh
+++ q1.sh
@@ -1,1 +1,7 @@
-This is q1 answer
+awk 'END {print NR}' aliceinwonderland.txt
+awk '{for (i=2;i<NF;i++) if($i=="Alice") count++} END {print count}' aliceinwonderland.txt
+# another option for B:
+# awk '/ Alice / {count++} END {print count}' aliceinwonderland.txt
+awk '{for (i=2; i<NF; i++) a[$i]++} END {for (k in a) if(a[k] ==1) print k}' aliceinwonderland.txt
+awk '{for(i=1;i<=NF;i++) a[$i]++ } END {for (k in a) print k, a[k]}' aliceinwonderland.txt  | sort -k 2 -h | tail -6 | sed '5d'
+awk '{for (i=1;i<=NF;i++) {total_length+=length($i);count++}} END {print total_length/count}' aliceinwonderland.txt
